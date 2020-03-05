5202bb8dc1253f253be8b2297d64b23585a65f26
ANA LUISA MORENO <almoreno@unah.hn>
2020-03-05 11:00:47 -0600

    categorias

--- Shopping Online.BL/ProductosBL.cs
+++ Shopping Online.BL/ProductosBL.cs
@@ -9,6 +9,7 @@ namespace Shopping_Online.BL
     public class ProductosBL
     {
         Contexto _contexto;
+
         public List<Producto> ListaDeProductos { get; set; }
 
         public ProductosBL()
@@ -23,6 +24,37 @@ namespace Shopping_Online.BL
            
 
             return ListaDeProductos;
+        }
+        public void GuardarProducto(Producto producto)
+        {
+            if (producto.Id == 0)
+            {
+                _contexto.Productos.Add(producto);
+            }
+            else
+            {
+                var productoExistente = _contexto.Productos.Find(producto.Id);
+
+                productoExistente.Descripcion = producto.Descripcion;
+                productoExistente.Precio = producto.Precio;
+            
+            }
+
+          
+            _contexto.SaveChanges();
+        }
+
+        public Producto ObtenerProducto(int id)
+        {
+            var producto = _contexto.Productos.Find(id);
+            return producto;
+        }
+        public void EliminarProducto(int id)
+        {
+            var producto = _contexto.Productos.Find(id);
+
+            _contexto.Productos.Remove(producto);
+            _contexto.SaveChanges();
         }
     }
 }
